3622dc749386cf20732470fd814ceadf949fc5f0
dong uk <gusrn1255@naver.com>
2021-01-18 23:44:06 +0900

    loarn_0118

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    // 1. 데이터 담을 공간
+    const [loans, setLoans] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //3. 네트워킹 함수
+    const getData = async () => {
+        return (
+            await axios.get('https://api.kivaws.org/v1/loans/newest.json')
+                 // .then(aaa => console.log(aaa.data.loans))
+                .then(aaa => {
+                    setLoans(aaa.data.loans)
+                    setLoading(false)
+                })
+                .catch(err => console.log(err))
+
+        )
+    }
+
+    //2. 자동실행 함수 useEffect
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ...
+                    </h1>
+                </div>
+            : (
+                    <div>
+                        {loans.map(loan => (
+                            <>
+                                <h1>{loan.name}</h1>
+                                <h2>{loan.status}</h2>
+                                <h3>{loan.loan_amount}</h3>
+                            </>
+                        ))}
+                    </div>
+
+                )
+            }
+        </>
+
+    );
+};
 
 export default App;
